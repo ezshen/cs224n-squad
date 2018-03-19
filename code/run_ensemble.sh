df173c79c9fd5d12b06a126aef978b03b9ce3d32
ezshen <ezshen@stanford.edu>
2018-03-19 00:59:18 -0700

    bash file fix, with rerun

--- code/run_ensemble.sh
+++ code/run_ensemble.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source_path=$1
-target_path=$2
+source_file=$1
+target_file=$2
+rerun=$3
 
 declare -a experiments=(
-                "bidaf_charcnn_train"
+                # "bidaf_charcnn_train"
                 "bidaf_charcnn_train1"
                 "bidaf_charcnn_train2"
                 # "bidaf_charcnn_train3"
@@ -15,13 +16,15 @@ declare -a experiments=(
 
 eargs=""
 for exp in "${experiments[@]}"; do
-    eval_path="../experiments/$exp/predictions_span.json"
+    eval_path="~/cs224n-squad/experiments/$exp/predictions_span.json"
     eargs="$eargs $eval_path"
-    echo "running official_eval for $exp..."
-    python main.py --experiment_name=$exp --mode=official_eval --json_in_path=$source_path --ckpt_load_dir=../experiments/$exp/best_checkpoint
+    if ["$rerun" ]
+    then
+        echo "running official_eval for $exp..."
+        python main.py --experiment_name=$exp --mode=official_eval --json_in_path=$~/cs224n-squad/data/$source_file --ckpt_load_dir=~/cs224n-squad/experiments/$exp/best_checkpoint
+    fi
 done
 wait
 
 # Ensemble
-python ensemble.py $eargs --json_in_path=$source_path --output_file=$target_path
-./run_ensemble.sh ~/cs224n-squad/tiny-dev.json ~/cs224n-squad/ensemble_predictions.json
+python ensemble.py $eargs --json_in_path=$source_path --output_file=~/cs224n-squad/$target_file
