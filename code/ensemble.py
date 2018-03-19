0c53156c5d146fde506f1fabcb6f9c4ed1050dac
Ubuntu <ezshen@cs224-dev-2.1arrhttdkbwerk3p42ygbfcavc.jx.internal.cloudapp.net>
2018-03-19 04:49:07 +0000

    ensembling should work

--- code/ensemble.py
+++ code/ensemble.py
@@ -9,8 +9,8 @@ from official_eval_helper import get_json_data, readnext
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('paths', nargs='+')
-    parser.add_argument('-o', '--output_file', default='../ensemble_predictions.json')
-    parser.add_argument("--json_in_path", default="../dev-v1.1.json")
+    parser.add_argument('-o', '--output_file', default='ensemble_predictions.json')
+    parser.add_argument("--json_in_path", default="dev-v1.1.json")
     args = parser.parse_args()
     return args
 
@@ -21,10 +21,11 @@ def get_tokens(token_data):
         if not tok:
             break
         tokens.append(tok)
+    return tokens
 
 def ensemble(args):
     qn_uuid_data, context_token_data, _ = get_json_data(args.json_in_path)
-
+    
     context_tokens = get_tokens(context_token_data)
     qn_uuid_tokens = get_tokens(qn_uuid_data)
     uuid2context = dict(zip(qn_uuid_tokens, context_tokens))
@@ -33,15 +34,18 @@ def ensemble(args):
     for path in tqdm(args.paths):
         with open(path, 'r') as fh:
             preds = json.load(fh)
-            for uuid, pred in preds.iteritems():
-                model_preds.setdefault(uuid, []).append(pred)
+            print preds
+	    for uuid, pred in preds.iteritems():
+                model_preds.setdefault(uuid, []).append(tuple(pred))
 
     assert len(context_tokens) == len(qn_uuid_tokens)
     assert len(qn_uuid_tokens) == len(model_preds)
 
     final_pred = {}
     for uuid, preds in tqdm(model_preds.iteritems()):
-        pred_start, pred_end = max_vote(preds)
+        print uuid, preds
+
+	pred_start, pred_end = max_vote(preds)
         final_pred[uuid] = uuid2context[uuid][pred_start: pred_end+1]
 
 def max_vote(preds):
