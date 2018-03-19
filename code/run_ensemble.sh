500061b2c01ef2ccc94ad814020e4e287414a9d3
Ubuntu <ezshen@cs224-dev-2.1arrhttdkbwerk3p42ygbfcavc.jx.internal.cloudapp.net>
2018-03-19 09:23:55 +0000

    add tiny-dev predictions

--- code/run_ensemble.sh
+++ code/run_ensemble.sh
@@ -7,10 +7,10 @@ declare -a experiments=(
                 # "bidaf_charcnn_train"
                 "bidaf_charcnn_train1"
                 "bidaf_charcnn_train2"
-                # "bidaf_charcnn_train3"
-                # "bidaf_charcnn_train4"
-                # "bidaf_charcnn_train5"
-                # "bidaf_charcnn_train6"
+                "bidaf_charcnn_train3"
+                "bidaf_charcnn_train4"
+                "bidaf_charcnn_train5"
+                "bidaf_charcnn_train6"
                 # "bidaf_charcnn_train_singlelayer1"
                 )
 
@@ -18,7 +18,7 @@ eargs=""
 for exp in "${experiments[@]}"; do
     eval_path="../experiments/$exp/predictions_span.json"
     eargs="$eargs $eval_path"
-    if [  $rerun]
+    if [ ! $rerun]
     then
         echo "running official_eval for $exp..."
         python main.py --experiment_name=$exp --mode=official_eval --json_in_path=../data/$source_file --ckpt_load_dir=../experiments/$exp/best_checkpoint
